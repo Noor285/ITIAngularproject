7c3e4b1757ac3db5dfc65feb73f6be990461333e
justomar21 <tarekes68@gmail.com>
2024-03-29 22:46:41 +0200

    added appointment page for patient

--- src/app/Components/Core/Patient/all-appointments/all-appointments.component.ts
+++ src/app/Components/Core/Patient/all-appointments/all-appointments.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { ISingleDocAppointment } from './../../../../Models/SingleDoctorAppointment';
+import { AfterViewInit, Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { IAppointment } from 'src/app/Models/appointment';
+import { DoctorService } from 'src/app/Services/doctor.service';
+import { PatientService } from 'src/app/Services/patient.service';
 
 @Component({
   selector: 'app-all-appointments',
   templateUrl: './all-appointments.component.html',
   styleUrls: ['./all-appointments.component.css']
 })
-export class AllAppointmentsComponent {
+export class AllAppointmentsComponent implements AfterViewInit {
+  constructor(private _PatientService:PatientService , private _DoctorService:DoctorService){}
+  ngAfterViewInit(): void {
 
+    this._PatientService.getAllAppointments(1).subscribe((res)=>{
+      this.appointmentDocs = res;
+      console.log(res);
+    });
+  }
+  appointmentDocs! : ISingleDocAppointment[];
+  id:any =1;
+  patientProfile:any;
+  rateDialog: boolean = false;
+  selectedRating:any;
+  additionalNotes: string = '';
+  rateForm!: FormGroup;
+
+  showDialog() {
+
+    this.rateDialog= true;
+  }
+
+
+  submitRating(form: any) {
+    // Process your form submission here
+    console.log("Submitted Rating:", this.selectedRating);
+    console.log("Additional Notes:", this.additionalNotes);
+    console.log(form.value);
+    // this._PatientService.postpatientRate(form.value).subscribe((res)=>{
+    //   console.log(res);
+    // });
+
+
+    // Reset the form after submission
+    form.resetForm();
+
+  }
 }
