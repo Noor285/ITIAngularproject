8e9358e149a6a53e6289d682d65b233be788b6d9
justomar21 <tarekes68@gmail.com>
2024-03-30 17:58:13 +0200

    updated navbar

--- src/app/Components/Shared/nav-bar/nav-bar.component.ts
+++ src/app/Components/Shared/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent {
-
+  constructor(private router : Router){}
+  role : string = localStorage.getItem("role") ?? "";
+  Logout()
+  {
+    localStorage.clear();
+    this.router.navigate(['/signin']);
+  }
 }
