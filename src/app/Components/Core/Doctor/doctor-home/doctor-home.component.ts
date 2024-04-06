2cea4abdfe920cfdbee5d64d9210d39771155874
justomar21 <tarekes68@gmail.com>
2024-04-06 13:18:05 +0200

    added authorizaiton

--- src/app/Components/Core/Doctor/doctor-home/doctor-home.component.ts
+++ src/app/Components/Core/Doctor/doctor-home/doctor-home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Status } from 'src/app/Enums/Status';
 
 @Component({
   selector: 'app-doctor-home',
@@ -9,4 +11,17 @@ export class DoctorHomeComponent {
 
     backgroundUrl: any = 'assets/home/Vector.png';
 
+    id:number = parseInt(localStorage.getItem("id") ?? "");
+    role:string = localStorage.getItem("role") ?? "";
+    constructor(private router:Router) { }
+    status: Status = parseInt(localStorage.getItem("status")??"") as Status;
+    ngOnInit(): void {
+        if (isNaN(this.id)) this.router.navigate(['signin']);
+        if (this.role != "doctor") {
+            this.router.navigate(['unauthorized']);
+        }
+        if (this.status == Status.Rejected) this.router.navigate(['doctor/rejected']);
+        if (this.status == Status.Inactive) this.router.navigate(['doctor/inactive']);
+        if (this.status == Status.Banned) this.router.navigate(['banned']);
+    }
 }
